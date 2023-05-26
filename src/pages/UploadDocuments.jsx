d346b5fd59f70cd0004a9890c797a8dabf5a05fa
YuvalBallas <yuvalballas2@gmail.com>
2023-05-27 01:57:17 +0300

    upload docs

---
+++ src/pages/UploadDocuments.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UploadDocuments = () => {
+    return (
+      <div>
+        <h2>Upload Document Page</h2>
+        {/* Add content for the Upload Document page */}
+      </div>
+    );
+};
+
+export default UploadDocuments;
