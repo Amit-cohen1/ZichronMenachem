41ceb8765254e6b75016a635c43689200095b7b5
danielpol245 <127850537+danielpol245@users.noreply.github.com>
2023-06-01 15:41:48 +0300

    up
    updte

--- src/pages/AfterReg.jsx
+++ src/pages/AfterReg.jsx
@@ -1,10 +1,60 @@
-import React from "react";
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { sendEmailVerification } from 'firebase/auth';
+import firebase , { auth } from '../firebase';
 
 const AfterReg = () => {
-    return (
-    <div className="container1">
+  const [resendDisabled, setResendDisabled] = React.useState(false);
+  const [countdown, setCountdown] = React.useState(60);
+  const [error, setError] = useState('');
+
+  const handleResend = async () => {
+    setError('');
+  
+    try {
+      const user = auth.currentUser;
+  
+      // Send email verification
+      await sendEmailVerification(user);
+  
+      console.log("Verification email sent successfully.");
+  
+      // Disable the button and start the countdown timer
+      setResendDisabled(true);
+      startCountdown();
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+  
+  
+  
+
+  const startCountdown = () => {
+    setResendDisabled(true);
+    setCountdown(60);
+  
+    const timer = setInterval(() => {
+      setCountdown((prevCountdown) => {
+        if (prevCountdown === 1) {
+          clearInterval(timer);
+          setResendDisabled(false);
+        }
+        return prevCountdown - 1;
+      });
+    }, 1000);
+  };
+  
+
+  return (
+    <div className="container-message">
       <h2>תודה שנרשמת! בבקשה אשר את המייל שנשלח אלייך ועבור לעמוד התחברות</h2>
+      <p>לא קיבלת מייל אישור ? לשליחה חוזרת לחץ שלח</p>
+      <p>
+      <button className='resendBtn' onClick={handleResend} disabled={resendDisabled}>
+        {resendDisabled ? `שלח (${countdown})` : 'שלח'}
+      </button>
+      </p>
       <p><Link to='/login'>מעבר לעמוד התחברות</Link></p>
     </div>
   );
