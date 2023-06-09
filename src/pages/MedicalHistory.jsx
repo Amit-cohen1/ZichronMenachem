2fee6a4254b3105f2b1fda533619a6a5a6463258
danielpol245 <127850537+danielpol245@users.noreply.github.com>
2023-06-09 15:44:01 +0300

    Update MedicalHistory.jsx

--- src/pages/MedicalHistory.jsx
+++ src/pages/MedicalHistory.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
+import Background from '../components/Background';
+import { getDoc } from 'firebase/firestore';
+import { collection, query, where, getDocs, getFirestore, updateDoc } from "firebase/firestore";
+const db = getFirestore();
 
 const MedicalHistory = () => {
-    return (
+  
+  const handlehistory = async (event) => {
+    const q = query(
+      collection(db, "Childrens"),
+      where("id", "==", event.target.value)
+    );
+    const qs = await getDocs(q);
+    qs.forEach(async (doc) => {
+      if (doc.exists) {
+        console.log(doc.data)
+    }});
+  const handleLogout = () => {
+    auth.signOut()
+      .then(() => {
+        console.log('User logged out successfully');
+        // Perform any additional cleanup or redirection logic here
+      })
+      .catch((error) => {
+        console.error('Error logging out:', error);
+      });
+  }; 
+  return (
       <div>
         <h2>Medical History</h2>
         {/* Add content for the medical history page */}
+
+        <div className="containerSearchBox">
+          <input className='searchBarAdmin' type="text" placeholder="חפש לפי תעודת זהות" value={searchTerm} onChange={handleSearchChange} />
+          <button className='AdminBtn1' type="button" value={searchTerm} onClick={handleSearchSubmit}>חפש</button>
+        </div>
+
       </div>
     );
 };
-
+}
 export default MedicalHistory;
