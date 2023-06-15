c9d6521c31b2ab9f9c92e7fefed5e74bee517aa0
Amit Cohen <75366645+Amit-cohen1@users.noreply.github.com>
2023-06-15 15:40:02 +0300

    Hananel Changes

---
+++ src/pages/UserEmailContainer.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { collection, query, where, getDocs, getFirestore, updateDoc } from "firebase/firestore";
+import './UserEmailContainer.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const db = getFirestore();
+
+const UserEmailContainer = ({ userEmail }) => {
+  const handleClick = async () => {
+    const selectedRole = document.getElementById("myS").value;
+    console.log(selectedRole);
+    console.log(userEmail);
+
+    const q = query(
+      collection(db, "users"),
+      where("email", "==", userEmail)
+    );
+    const qs = await getDocs(q);
+    qs.forEach(async (doc) => {
+      if (doc.exists) {
+        await updateDoc(doc.ref, {
+          role: selectedRole
+        });
+        toast.success('Role changed successfully');
+      }
+    });
+  };
+
+  return (
+    <div className="user-email-container">
+      <div className="user-email">{userEmail}</div>
+      <div className="role-dropdown">
+        <label className="dropdown-label" >
+          בחר תפקיד:
+          <select id="myS" className="dropdown-select">
+            <option value="admin">Admin</option>
+            <option value="medicalStaff">Medical Staff</option>
+            <option value="parent">Parent</option>
+          </select>
+        </label>
+      </div>
+      <button className="apply-button" onClick={handleClick} >
+        החל
+      </button>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default UserEmailContainer;
